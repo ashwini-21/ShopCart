93e319310105813cd5e43196094bb17c2a7d7308
ashwini-21 <145797231+ashwini-21@users.noreply.github.com>
2024-12-20 13:42:52 -0500

    First Commit and push

---
+++ MyShopCart/MyShopCart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyShopCart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
